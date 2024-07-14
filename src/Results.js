b890e3989ed4d9df93becf0dbb334a799c336b66
DryadNymph <mymind203@yahoo.com>
2024-07-14 20:32:55 +0200

    Added dictionary.css

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   console.log(props);
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <section>
+          <h2>{props.results.word}</h2>
 
-        <div>
-          <Phonetic phonetic={props.results.phonetic} />
-        </div>
+          <div>
+            <Phonetic phonetic={props.results.phonetic} />
+          </div>
+        </section>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
